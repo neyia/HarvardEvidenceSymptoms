0aaf0efbe04eaea58347f9628b7dd715468bc5d5
Natallia <nzhyrkova@biarum.com>
2017-01-21 19:29:12 +0300

    included necessary plugs

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,21 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer'),
     processhtml = require('gulp-processhtml'),
     less = require('gulp-less'),
-    path = require('path');
+    path = require('path'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    watch = require('gulp-watch');
+
+var config = {
+    server: {
+        baseDir: "build"
+    },
+    tunnel: true,
+    host: 'evidence',
+    port: 9999,
+    injectChanges: true,
+    logPrefix: "App Evidence"
+};
 
 /* HTML */
 gulp.task('html', function () {
@@ -13,12 +27,14 @@ gulp.task('html', function () {
         .pipe(processhtml({
             recursive: true
         }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'))
+        .pipe(reload({stream: true}));
 });
 /* JavaScript */
 gulp.task('js', function () {
     return gulp.src('src/scripts/*.js')
-        .pipe(gulp.dest('build/scripts/'));
+        .pipe(gulp.dest('build/scripts/'))
+        .pipe(gulp.dest('build/scripts/')).pipe(reload({stream: true}));
 });
 /* Styles */
 gulp.task('less', function () {
@@ -30,5 +46,16 @@ gulp.task('less', function () {
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('build/styles'));
+        .pipe(gulp.dest('build/styles'))
+        .pipe(reload({stream: true}));
 });
+
+/* BrowserSync local web server*/
+gulp.task('browser-sync', function () {
+    browserSync(config);
+});
+gulp.task('watch', function () {
+    gulp.watch('src/*.html',['html']);
+    gulp.watch('src/styles/*',['less']);
+});
+gulp.task('default', ['js', 'html', 'less', 'browser-sync', 'watch']);
