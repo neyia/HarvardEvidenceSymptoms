5ba9048c6d7fc9907115b356b3d6d56f3214249a
Natallia <nzhyrkova@biarum.com>
2017-01-21 15:49:46 +0300

    added gulp necessary plugins

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,23 @@
  * Created by nzhyrkova on 21.01.2017.
  */
 var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer'),
+    processhtml = require('gulp-processhtml');
 
+/* HTML */
+gulp.task('html', function () {
+    return gulp.src('src/*.html')
+        .pipe(processhtml({
+            recursive: true
+        }))
+        .pipe(gulp.dest('build/'));
+});
+/* JavaScript */
+gulp.task('js', function () {
+    return gulp.src('src/scripts/*.js')
+        .pipe(gulp.dest('build/scripts/'));
+});
+/* Styles */
 gulp.task('autoprefixer', function () {
     return gulp.src('src/styles/common.css')
         .pipe(autoprefixer({
