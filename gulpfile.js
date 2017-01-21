4bff7362993ea500f84797efcbd7a33fe2880bbf
Natallia <nzhyrkova@biarum.com>
2017-01-21 15:26:29 +0300

    added settings, npm, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 /**
  * Created by nzhyrkova on 21.01.2017.
  */
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('autoprefixer', function () {
+    return gulp.src('src/styles/common.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('build/styles/common.css'));
+});
